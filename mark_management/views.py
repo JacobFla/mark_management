5606e25fbb6358a8ad635a296fa52493af26a013
Ilja Bauer <i.bauer@its-bh.de>
2014-05-15 09:40:48 +0200

    restructured project

--- mark_management/views.py
+++ mark_management/views.py
@@ -1,3 +1,13 @@
+# coding=utf-8
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    #Objekte aus der Datenbank holen, mittelwert berechne siehe Subject.get_average()
+
+    #Formular erstellen
+
+    #Alles in den Kontext einfügen
+    context = {}
+
+    return render(request, "mark_management/index.html", context)
